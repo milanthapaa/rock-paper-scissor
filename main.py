e342c6589091e9770c95c5f6be06cac73c8574ce
Milan Thapa <45590593+milanthapaa@users.noreply.github.com>
2022-05-22 18:45:08 +0545

    added win and loss condition

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ def rock_paper_scissor():
     computer = random.choice(possible_values)
 
     while True:
-        user = input("Choose one from Rock, Paper and Scissor").lower()
+        user = input("Choose one from Rock, Paper and Scissor: ").lower()
+        if computer == "rock" and user == "rock":
+            print("TRY AGAIN!")
+        elif computer == "rock" and user == "scissor" or \
+                computer == "scissor" and user == "paper" or \
+                computer == "paper" and user == "rock":
+            print("COMPUTER WON!")
+            break
+        else:
+            print("YOU'VE WON!")
+            break
 
 
 rock_paper_scissor()
