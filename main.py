dee296e3c140b7b921106dd7c24742fc304a7d3f
Milan Thapa <45590593+milanthapaa@users.noreply.github.com>
2022-05-22 18:38:45 +0545

    setting computer and user inputs

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import random
 
 
 def rock_paper_scissor():
-    pass
+    print("Welcome to Rock, Paper and Scissor Game")
+    possible_values = ["rock", "paper", "scissor"]
+    computer = random.choice(possible_values)
+
+    while True:
+        user = input("Choose one from Rock, Paper and Scissor").lower()
+
+
+rock_paper_scissor()
