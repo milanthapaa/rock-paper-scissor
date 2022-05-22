821745444042a9c33e2a46c8e50e7dbec278998a
Milan Thapa <45590593+milanthapaa@users.noreply.github.com>
2022-05-22 20:31:53 +0545

    added user input validation

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ def rock_paper_scissor():
 
     while True:
         user = input("Choose one from Rock, Paper and Scissor: ").lower()
-        if computer == "rock" and user == "rock":
-            print("TRY AGAIN!")
-        elif computer == "rock" and user == "scissor" or \
-                computer == "scissor" and user == "paper" or \
-                computer == "paper" and user == "rock":
-            print("COMPUTER WON!")
-            break
+        if user in possible_values:
+            if computer == "rock" and user == "rock":
+                print("TRY AGAIN!")
+            elif computer == "rock" and user == "scissor" or \
+                    computer == "scissor" and user == "paper" or \
+                    computer == "paper" and user == "rock":
+                print("COMPUTER WON!")
+                break
+            else:
+                print("YOU'VE WON!")
+                break
         else:
-            print("YOU'VE WON!")
-            break
+            print("Add on of the correct input (rock, paper, scissor)")
 
 
 rock_paper_scissor()
